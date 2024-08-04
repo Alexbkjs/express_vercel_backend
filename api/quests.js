b3b11f476f6e3c2e8fbfdda8ed862c845dbabdb0
Alexbkjs <bakaaleksandr1194@gmail.com>
2024-08-04 12:04:30 +0300

    attemt to use aws s3 for storing images

--- api/quests.js
+++ api/quests.js
@@ -1,42 +1,42 @@
 export const quests = [
     {
         "id": 1,
-        "imageUrl": "/src/images/quests/basic.png",
+        "imageUrl": "https://quests-app-bucket.s3.eu-north-1.amazonaws.com/images/basic.png",
         "name": "Базовий квест",
         "description": "Візит до Академії для отримання першого рівня та вибору базового класу",
         "award": "очки досвіду, підвищення класу"
     },
     {
         "id": 2,
-        "imageUrl": "/src/images/quests/routine.png",
+        "imageUrl": "https://quests-app-bucket.s3.eu-north-1.amazonaws.com/images/routine.png",
         "name": "Рутинний квест",
         "description": "Виконання тестового завдання для отримання артефакту та Мідних Монет",
         "award": "очки досвіду, мідні монети, артефакти"
     },
     {
         "id": 3,
-        "imageUrl": "/src/images/quests/adventure.png",
+        "imageUrl": "https://quests-app-bucket.s3.eu-north-1.amazonaws.com/images/adventure.png",
         "name": "Пригодницький квест",
         "description": "Проходження кар’єрного коучинга,навчальних курсів та подібних завдань",
         "award": "підвищення класу, рівня,артефакти, мідні монети"
     },
     {
         "id": 4,
-        "imageUrl": "/src/images/quests/reusable.png",
+        "imageUrl": "https://quests-app-bucket.s3.eu-north-1.amazonaws.com/images/reusable.png",
         "name": "Багаторазовий квест",
         "description": "Залучайте нових учасників за партнерським посиланням",
         "award": "очки досвіду"
     },
     {
         "id": 5,
-        "imageUrl": "/src/images/quests/team.png",
+        "imageUrl": "https://quests-app-bucket.s3.eu-north-1.amazonaws.com/images/team.png",
         "name": "Командний квест",
         "description": "Участь у командному проекті для отримання досвіду та Золотих Монет",
         "award": "очки досвіду, золоті тамідні монети, артефакти"
     },
     {
         "id": 6,
-        "imageUrl": "/src/images/quests/mini_boss.png",
+        "imageUrl": "https://quests-app-bucket.s3.eu-north-1.amazonaws.com/images/mini_boss.png",
         "name": "Битва з міні-босом",
         "description": "Складання онлайн-іспиту для отримання артефакту",
         "award": "артефакти"
