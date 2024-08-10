574605790e1c696e5dd3b48a2670e46e93d1c306
Alexbkjs <bakaaleksandr1194@gmail.com>
2024-08-10 17:55:19 +0300

    homepage url

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 
@@ -22,29 +21,29 @@ app.get("/api/v1/quests", (req, res) => {
 });
 
 
-// // Endpoint to fetch quests with pagination
-// app.get("/api/v1/quests/images", (req, res) => {
-//   const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
-//   const pageSize = 3; // Number of quests per page, adjust as needed
+// Endpoint to fetch quests with pagination
+app.get("/api/v1/quests/images", (req, res) => {
+  const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
+  const pageSize = 3; // Number of quests per page, adjust as needed
 
-//   const startIndex = (page - 1) * pageSize;
-//   const endIndex = startIndex + pageSize;
-//   const paginatedQuests = quests.slice(startIndex, endIndex);
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedQuests = quests.slice(startIndex, endIndex);
 
-//   res.json(paginatedQuests);
-// });
+  res.json(paginatedQuests);
+});
 
-// // Endpoint to fetch quests with pagination
-// app.get("/api/v1/quests/text", (req, res) => {
-//   const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
-//   const pageSize = 3; // Number of quests per page, adjust as needed
+// Endpoint to fetch quests with pagination
+app.get("/api/v1/quests/text", (req, res) => {
+  const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
+  const pageSize = 3; // Number of quests per page, adjust as needed
 
-//   const startIndex = (page - 1) * pageSize;
-//   const endIndex = startIndex + pageSize;
-//   const paginatedQuests = quests.slice(startIndex, endIndex);
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedQuests = quests.slice(startIndex, endIndex);
 
-//   res.json(paginatedQuests);
-// });
+  res.json(paginatedQuests);
+});
 
 
 
