e1c4e36bc79b4ba3df075fb6a9ef387264d9d74b
Alexbkjs <bakaaleksandr1194@gmail.com>
2024-08-10 18:03:41 +0300

    convert to es6

--- api/index.js
+++ api/index.js
@@ -1,29 +1,14 @@
-const express = require("express");
-const app = express();
+import express from 'express';
+import cors from 'cors';
+import { quests } from './quests.js'; // Adjust the import based on which file you need
 
-const cors = require('cors');
+const app = express();
 
 app.use(cors()); // Allow all origins
 
-// const { quests } = require('./quests_cldn.js');
-const { quests } = require('./quests.js');
-
 // Endpoint to fetch quests with pagination
-app.get("/api/v1/quests", (req, res) => {
-  const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
-  const pageSize = 3; // Number of quests per page, adjust as needed
-
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-  const paginatedQuests = quests.slice(startIndex, endIndex);
-
-  res.json(paginatedQuests);
-});
-
-
-// Endpoint to fetch quests with pagination
-app.get("/api/v1/quests/images", (req, res) => {
-  const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
+app.get('/api/v1/quests', (req, res) => {
+  const page = parseInt(req.query.page, 10) || 1; // Default to page 1 if not specified
   const pageSize = 3; // Number of quests per page, adjust as needed
 
   const startIndex = (page - 1) * pageSize;
@@ -34,8 +19,8 @@ app.get("/api/v1/quests/images", (req, res) => {
 });
 
 // Endpoint to fetch quests with pagination
-app.get("/api/v1/quests/text", (req, res) => {
-  const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
+app.get('/api/v1/quests/images', (req, res) => {
+  const page = parseInt(req.query.page, 10) || 1; // Default to page 1 if not specified
   const pageSize = 3; // Number of quests per page, adjust as needed
 
   const startIndex = (page - 1) * pageSize;
@@ -45,6 +30,16 @@ app.get("/api/v1/quests/text", (req, res) => {
   res.json(paginatedQuests);
 });
 
+// Endpoint to fetch quests with pagination
+app.get('/api/v1/quests/text', (req, res) => {
+  const page = parseInt(req.query.page, 10) || 1; // Default to page 1 if not specified
+  const pageSize = 3; // Number of quests per page, adjust as needed
 
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedQuests = quests.slice(startIndex, endIndex);
 
-module.exports = app;
+  res.json(paginatedQuests);
+});
+
+export default app;
