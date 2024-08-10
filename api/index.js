95ae77c3dbcc6bd2863a16ba719157ae94505be5
Alexbkjs <bakaaleksandr1194@gmail.com>
2024-08-10 17:50:29 +0300

    trying to load quests from back

--- api/index.js
+++ api/index.js
@@ -9,9 +9,8 @@ app.use(cors()); // Allow all origins
 // const { quests } = require('./quests_cldn.js');
 const { quests } = require('./quests.js');
 
-
 // Endpoint to fetch quests with pagination
-app.get("/api/v1/quests/images", (req, res) => {
+app.get("/api/v1/quests", (req, res) => {
   const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
   const pageSize = 3; // Number of quests per page, adjust as needed
 
@@ -22,17 +21,30 @@ app.get("/api/v1/quests/images", (req, res) => {
   res.json(paginatedQuests);
 });
 
-// Endpoint to fetch quests with pagination
-app.get("/api/v1/quests/text", (req, res) => {
-  const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
-  const pageSize = 3; // Number of quests per page, adjust as needed
 
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-  const paginatedQuests = quests.slice(startIndex, endIndex);
+// // Endpoint to fetch quests with pagination
+// app.get("/api/v1/quests/images", (req, res) => {
+//   const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
+//   const pageSize = 3; // Number of quests per page, adjust as needed
 
-  res.json(paginatedQuests);
-});
+//   const startIndex = (page - 1) * pageSize;
+//   const endIndex = startIndex + pageSize;
+//   const paginatedQuests = quests.slice(startIndex, endIndex);
+
+//   res.json(paginatedQuests);
+// });
+
+// // Endpoint to fetch quests with pagination
+// app.get("/api/v1/quests/text", (req, res) => {
+//   const page = parseInt(req.query.page) || 1; // Default to page 1 if not specified
+//   const pageSize = 3; // Number of quests per page, adjust as needed
+
+//   const startIndex = (page - 1) * pageSize;
+//   const endIndex = startIndex + pageSize;
+//   const paginatedQuests = quests.slice(startIndex, endIndex);
+
+//   res.json(paginatedQuests);
+// });
 
 
 
